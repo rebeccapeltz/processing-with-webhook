009f00145c95b05c14173b7960a1fd1ac868ac23
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-02-04 10:03:04 -0800

    use async/await for functions

--- functions/notify.js
+++ functions/notify.js
@@ -1,8 +1,7 @@
-
 exports.handler = async function (event, context) {
-    require('dotenv').config();
-
+  require('dotenv').config();
   const sgMail = require('@sendgrid/mail');
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   console.log(process.env.SENDGRID_API_KEY);
 
@@ -14,26 +13,26 @@ exports.handler = async function (event, context) {
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
 
-  sgMail
-    .send(msg)
-    .then((response) => {
-      console.log('success', response[0].code);
-      console.log(response[0].headers);
-      console.log('returning 200');
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response),
-      };
-    })
-    .catch((error) => {
-      console.error('error', JSON.stringify(error, 0, 2));
-      console.log(error.code);
-      console.log(error.response.body);
-      const errorMsg = error.response.body.errors[0].message;
-      console.log(errorMsg)
-      return {
-        statusCode: error.code,
-        body: errorMsg
-      };
-    });
+
+  try {
+    const response = await sgMail.send(msg);
+    console.log('success', response[0].statusCode);
+    console.log(response[0]);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: response[0] }),
+    };
+  } catch (error) {
+    console.error('error', JSON.stringify(error, 0, 2));
+    console.log(error.code);
+    console.log(error.response.body);
+    const errorMsg = error.response.body.errors[0].message;
+    console.log(errorMsg);
+
+    return {
+      statusCode: error.code,
+      body: errorMsg,
+    };
+  }
 };
